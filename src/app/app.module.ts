02095af89247255c14b1ea53cfbaec5e971d08d9
aminjlassi1 <mihamedamin.jlassi@sesame.com.tn>
2024-11-20 13:17:37 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms'; 
 import { AppComponent } from './app.component';
+import { CodeComponent } from './code/code.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CodeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
